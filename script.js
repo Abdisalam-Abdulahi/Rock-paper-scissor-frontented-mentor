d501f0c17ae418f42b81fe2362dfb945d3fe2c01
unknown <iidlex99@gmail.com>
2022-12-26 12:32:26 -0800

    commits

--- script.js
+++ script.js
@@ -6,50 +6,43 @@ const paper = document.querySelector("#paper");
 const rock = document.querySelector("#rock");
 const lizard = document.querySelector("#lizard");
 const spock = document.querySelector("#spock");
+const mainDiv = document.querySelector(".main-div");
+const clicked = document.querySelector(".clicked");
+const computerChoice = document.querySelector(".computerChoice");
 rulesBtn.addEventListener("click", () => {
   rules.style.display = "block";
 });
 closeIcon.addEventListener("click", () => {
   rules.style.display = "none";
 });
-// const iconsInfo = [
-//   {
-//     name: "scissors",
-//     src: "images/icon-scissors.svg",
-//   },
-//   {
-//     name: "paper",
-//     src: "images/icon-paper.svg",
-//   },
-//   {
-//     name: "rock",
-//     src: "images/icon-rock.svg",
-//   },
-//   {
-//     name: "scissors",
-//     src: "images/icon-lizard.svg",
-//   },
-//   {
-//     name: "spock",
-//     src: "images/icon-spock.svg",
-//   },
-// ];
+
 const stayer = [];
-function moveOthers(icon){
-    stayer.push(icon)
-   iconArray.forEach(Element=>{
-    if(!stayer.includes(Element)){
-        console.log(Element)
-        Element.style.display ="none"
-        // icon.style.display = "none"
+function moveOthers(icon) {
+  stayer.push(icon);
+  iconArray.forEach((Element) => {
+    if (!stayer.includes(Element)) {
+      Element.style.display = "none";
+      mainDiv.style.background = "var(--RadialGradient)";
+      clicked.appendChild(icon);
+      icon.style.height = "180px";
+      icon.style.width = "180px";
     }
-   })
-   }
+  });
+}
 
 const iconArray = [scissors, rock, paper, lizard, spock];
 iconArray.forEach((item) => {
   item.addEventListener("click", () => {
-    moveOthers(item)
-    
+    moveOthers(item);
+    randomGenarator(iconArray);
   });
 });
+function randomGenarator(array) {
+  const randomlyGenarated = Math.floor(Math.random() * array.length);
+  const randomDiv = array[randomlyGenarated];
+  randomDiv.style.display = "flex";
+  computerChoice.appendChild(randomDiv);
+  randomDiv.style.width = "180px";
+  randomDiv.style.height = "180px";
+  randomDiv.style.borderRadius = "50%";
+}
