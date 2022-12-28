86e8ac2dbbfa45e9ecc230c51b8b87d047d1474e
unknown <iidlex99@gmail.com>
2022-12-27 22:41:11 -0800

    commits

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ const mainDiv = document.querySelector(".main-div");
 const clicked = document.querySelector(".clicked");
 const computerChoice = document.querySelector(".computerChoice");
 const resultTxt = document.querySelector(".resultTxt");
+const scissorsClass = document.querySelector(".scissors");
+const paperClass = document.querySelector(".paper");
+const rockClass = document.querySelector(".rock");
+const lizardClass = document.querySelector(".lizard");
+const spockClass = document.querySelector(".spock");
 rulesBtn.addEventListener("click", () => {
   rules.style.display = "block";
 });
@@ -32,6 +37,13 @@ function moveOthers(icon) {
 }
 
 const iconArray = [scissors, rock, paper, lizard, spock];
+const classesArry = [
+  scissorsClass,
+  rockClass,
+  paperClass,
+  lizardClass,
+  spockClass,
+];
 const clickedarr = stayer;
 const computerarr = [];
 
@@ -39,9 +51,11 @@ iconArray.forEach((item) => {
   item.addEventListener("click", () => {
     moveOthers(item);
     randomGenarator(iconArray);
+
+    appender();
   });
 });
-console.log(computerarr);
+
 function randomGenarator(array) {
   const randomlyGenarated = Math.floor(Math.random() * array.length);
   const randomDiv = array[randomlyGenarated];
@@ -55,5 +69,22 @@ function randomGenarator(array) {
   if ((resultTxt.style.display = "block")) {
     clicked.style.left = "30%";
     computerChoice.style.left = "62%";
+  }
+}
+function appender() {
+  const computerId = computerarr[0].id;
+  const clickedId = clickedarr[0].id;
+  const imgsrc = computerarr[0].children[0].src;
+  if (computerId == clickedId) {
+    console.log("matched");
+    const div = document.createElement("div");
+    div.setAttribute("id", computerId);
+    div.setAttribute("class", "iconHolder");
+    const img = document.createElement("img");
+    img.setAttribute("src", imgsrc);
+    div.appendChild(img);
+    div.style.width = "180px";
+    div.style.height = "180px";
+    clicked.appendChild(div);
   }
 }
